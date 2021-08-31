9b262d34ed59f96c97424d4c67fe4b4eb15f37e0
EzequielSosa99 <82736044+EzequielSosa99@users.noreply.github.com>
2021-08-31 18:26:29 -0300

    TP1
    TP1

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <scanner.h>    // incluyo el .h, y no el .c
+#include  <stdlib.h>      // solo la uso aca esta libreria, entonces no va estrictamente en el .h
+
+
+int  main ()
+{
+    get_token();
+
+    return EXIT_SUCCESS;
+}
